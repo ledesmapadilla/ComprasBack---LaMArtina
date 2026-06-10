68bd6a0a534b722101a22457242732528d3eeac4
IgnacioLP <ignaciolp@lepa.com.ar>
2026-06-09 21:08:44 -0300

    fix: sacar autenticacion de rutas usuarios por ahora

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import { login, getAll, crear, actualizar, borrar } from "../controllers/usuario.controller.js";
-import { verificarToken, soloRoles } from "../middleware/auth.js";
 
 const router = Router();
 
 router.post("/login", login);
-router.get("/", verificarToken, soloRoles("superadmin"), getAll);
-router.post("/", verificarToken, soloRoles("superadmin"), crear);
-router.put("/:id", verificarToken, soloRoles("superadmin"), actualizar);
-router.delete("/:id", verificarToken, soloRoles("superadmin"), borrar);
+router.get("/", getAll);
+router.post("/", crear);
+router.put("/:id", actualizar);
+router.delete("/:id", borrar);
 
 export default router;
