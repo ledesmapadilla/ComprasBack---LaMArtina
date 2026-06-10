eedccb78967276382f68411f1775a835a42d277f
IgnacioLP <ignaciolp@lepa.com.ar>
2026-06-09 21:16:35 -0300

    fix: sacar import login que no existe en controller

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { login, getAll, crear, actualizar, borrar } from "../controllers/usuario.controller.js";
+import { getAll, crear, actualizar, borrar } from "../controllers/usuario.controller.js";
 
 const router = Router();
 
-router.post("/login", login);
 router.get("/", getAll);
 router.post("/", crear);
 router.put("/:id", actualizar);
